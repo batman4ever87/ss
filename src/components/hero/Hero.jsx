600d5ec2d1c3ea605a8d3832b496d4e3ef0d080a
Johannes Kujanpää <b61648764@gmail.com>
2024-06-12 16:12:16 +0300

    Initial commit

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -7,7 +7,19 @@ const Hero = () => {
   return (
    <section className="hero" id="hero">
      <div className="wrapper">
-      Hero
+      <div className="heroContainer">
+        <div className="heroContainer-box">
+          <h2 className="heroContainer-box-subtitle">HP-Pinnat OY</h2>
+          <h1 className="heroContainer-box-title">Tasoitetöitä <span>ammattitaidolla</span></h1>
+          <p className="heroContainer-box-text">
+            Me hoidamme saneeraus- ja uudisrakennushaasteesi. Teemme työmme huolella, jotta sinulla on yksi asia vähemmän huolehdittavaa.
+          </p>
+          <div className="heroContainer-box-buttons">
+            <button className="cta">Ota yhteyttä</button>
+            <button className="cta c2">Palvelut</button>
+          </div>
+        </div>
+      </div>
      </div>
    </section>
   )
